121438163b1b23fedec97ee8628b3331bf7447eb
mungdori <mungdori999@gmail.com>
2025-11-27 14:36:27 +0900

    first commit

---
+++ src/main/java/com/mungdori/napgame/NapGameApplication.java
@@ -0,0 +1,13 @@
+package com.mungdori.napgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NapGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NapGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mungdori/napgame/NapGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mungdori.napgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NapGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
